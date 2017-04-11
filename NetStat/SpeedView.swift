5d7ff937ae7e6c54338a84bc2b11410ba6a734f9
Ashwini Purohit <ashwinipurohit@yahoo.in>
2017-04-11 17:33:19 +0530

    basic UI implemented

---
+++ NetStat/SpeedView.swift
@@ -0,0 +1,23 @@
+//
+//  SpeedView.swift
+//  NetStat
+//
+//  Created by Ashwini Purohit on 11/04/17.
+//  Copyright © 2017 GDG. All rights reserved.
+//
+
+import Cocoa
+
+class SpeedView: NSView {
+
+    @IBOutlet weak var uploadText: NSTextField!
+    @IBOutlet weak var downloadText: NSTextField!
+    @IBAction func TestButton(_ sender: NSButton) {
+    }
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
--- NetStat/StatusMenuController.swift
+++ NetStat/StatusMenuController.swift
@@ -25,6 +25,35 @@ class StatusMenuController: NSObject {
         icon?.isTemplate = true // best for dark mode
         statusItem.image = icon
         statusItem.menu = statusMenu
+        
+        let reachability = Reachability()
+        reachability?.whenReachable = { reachability in
+            // this is called on a background thread, but UI updates must
+            // be on the main thread, like this:
+            DispatchQueue.main.async {
+                if reachability.isReachableViaWiFi {
+                    print("Reachable via WiFi")
+                    
+                } else {
+                    print("Reachable via Cellular")
+                }
+            }
+        }
+        reachability?.whenUnreachable = { reachability in
+            // this is called on a background thread, but UI updates must
+            // be on the main thread, like this:
+            DispatchQueue.main.async {
+                print("Not reachable")
+            }
+        }
+        
+        do {
+            try reachability?.startNotifier()
+        } catch {
+            print("Unable to start notifier")
+        }
+        
+        
 
     }
 }
